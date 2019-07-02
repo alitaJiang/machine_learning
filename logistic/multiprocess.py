eb86d31367e3b0b596c934d7fad273aaa11f88ee
Ace <aslanjiangwill@gmail.com>
2019-07-02 15:03:20 +0800

    add logistic

---
+++ logistic/multiprocess.py
@@ -0,0 +1,46 @@
+from multiprocessing import Pool
+import time
+
+
+def sha(fn):
+    def wrap(*args, **kwargs):
+        print('start to process')
+        print(fn(*args, **kwargs))
+        print('finished')
+
+    return wrap
+
+# sumAll = sha(sumAll)
+
+@sha
+def sumAll(*args, **kwargs):
+    a = 0
+    for i in args:
+        a+=i
+    return a
+
+def dec1(func):
+    print("1111")
+    def one():
+        print("2222")
+        func()
+        print("3333")
+    print('傻逼拉')
+    return one
+
+def dec2(func):
+    print("aaaa")
+    def two():
+        print("bbbb")
+        func()
+        print("cccc")
+    print('汤老师')
+    return two
+
+@dec1
+@dec2
+def test():
+    print("test test")
+
+
+
